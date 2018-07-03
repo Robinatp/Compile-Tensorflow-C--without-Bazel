d8bf50e9f6abdd2e4f8f84d8c6dd08fe1eeff8c0
Robin.Zhang <zhangbinatp@gmail.com>
2018-07-03 10:54:34 +0800

    Update copy_lib.sh

--- tensorflow_cpp/lib/copy_lib.sh
+++ tensorflow_cpp/lib/copy_lib.sh
@@ -1,9 +1,11 @@
 export TENSORFLOW_DIR=/workspace/software/tensorflow
 sudo cp -r $TENSORFLOW_DIR/bazel-bin/tensorflow/libtensorflow.so ./
 sudo cp -r $TENSORFLOW_DIR/bazel-bin/tensorflow/libtensorflow_cc.so ./
-sudo cp -r $TENSORFLOW_DIR/bazel-bin/tensorflow/libtensorflow_framework.so ./
+#sudo cp -r $TENSORFLOW_DIR/bazel-bin/tensorflow/libtensorflow_framework.so ./
+#bazel build //tensorflow:libtensorflow_cc.so
+#bazel build -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --copt=-msse4.1 --copt=-msse4.2 --config=monolithic //tensorflow:libtensorflow_cc.so
 sudo cp -r $TENSORFLOW_DIR/bazel-bin/tensorflow/libtensorflow.so /usr/local/lib
 sudo cp -r $TENSORFLOW_DIR/bazel-bin/tensorflow/libtensorflow_cc.so /usr/local/lib
-sudo cp -r $TENSORFLOW_DIR/bazel-bin/tensorflow/libtensorflow_framework.so /usr/local/lib
+#sudo cp -r $TENSORFLOW_DIR/bazel-bin/tensorflow/libtensorflow_framework.so /usr/local/lib
 
 
